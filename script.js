3b7fadb69ad5e10459756c7e7beb5e2fba1edb16
JaMagik <114476897+JaMagik@users.noreply.github.com>
2023-11-07 07:55:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -515,7 +515,83 @@ break;
   </table>
   `;
   break;
-        } else if (power === '8 kW') {
+        } 
+        else if (power === '14 kW') {
+            tableContent = `
+        <table id="customTable" border="1">
+        <tbody>
+            <tr>
+                <th>Numer</th>
+                <th>Nazwa towaru</th>
+                <th>Miara</th>
+                <th>Ilość</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td> JEDNOSTKA WEWNĘTRZNA TOSHIBA 14 KW 3F 1101HRW-E 1101XWH**W-E</td>
+                <td>szt.</td>
+                <td>1</td>
+            </tr>
+            <tr>s
+                <td>2</td>
+                <td>JEDNOSTKA ZEWNĘTRZNA TOSHIBA 14 KW 1101HRW-E</td>
+                <td>szt.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>ZASOBNIK CIEPŁEJ WODY UŻYTKOWEJ 300L</td>
+                <td>szt.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>BUFFOR (SPRZĘGŁO HYDRAULICZNE ) 40-140L + OSPRZET</td>
+                <td>szt.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>ELEMENTY HYDRAULICZNE I ELEKTRYCZNE DO POMPY CIEPŁA</td>
+                <td>kpl.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>Grupa bezpieczeństwa CWU (6bar)</td>
+                <td>szt.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td>MIEDŹ CHŁODNICZA</td>
+                <td>kpl.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td>GRUPA BEZPIECZEŃSTWA C.O (3 bar)</td>
+                <td>szt.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>9</td>
+                <td>STOJAK LUB WIESZAK POD POMPĘ CIEPŁA</td>
+                <td>szt.</td>
+                <td>1</td>
+            </tr>
+            <tr>
+                <td>10</td>
+                <td>MONTAŻ , DOJAZD , URUCHOMIENIE ORAZ SZKOLENIE UŻYTKOWNIKA</td>
+                <td>kpl.</td>
+                <td>1</td>
+            </tr>
+        </tbody>
+    </table>
+    `;
+    break;
+          } 
+        else if (power === '8 kW') {
           tableContent = `
       <table id="customTable" border="1">
       <tbody>
