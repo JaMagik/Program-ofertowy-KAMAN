e678d2889e7db75873dcc853571d65aebc54dea7
JaMagik <114476897+JaMagik@users.noreply.github.com>
2024-07-27 08:33:02 +0200

    1

--- script.js
+++ script.js
@@ -1031,6 +1031,91 @@ break;
 
 
     }
+    else if (power === '4 kW') {
+        tableContent = `
+    <table id="customTable" border="1">
+    <tbody>
+    <tr>
+    <th>Numer</th>
+    <th>Nazwa towaru</th>
+    <th>Miara</th>
+    <th>Ilość</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>JEDNOSTKA WEWNĘTRZNA MITSUBISHI SUZ HYPER HEATING 4 kW</td>
+    <td>szt.</td>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>JEDNOSTKA ZEWWEWNĘTRZNA MITSUBISHI SUZ HYPER HEATING 4 kW</td>
+    <td>szt.</td>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    ${tankRow}
+  </tr>
+  <tr>
+      <td>4</td>
+      ${bufferRow}
+  </tr>;
+  <tr>
+    <td>5</td>
+    <td>ELEMENTY HYDRAULICZNE I ELEKTRYCZNE DO POMPY CIEPŁA</td>
+    <td>kpl.</td>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>6</td>
+    <td>Grupa bezpieczeństwa CWU (6bar)</td>
+    <td>szt.</td>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>7</td>
+    <td>Grupa bezpieczeństwa C.0 (2.5 bar)</td>
+    <td>szt.</td>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>8</td>
+    <td>Pompa obiegowa do instalacji grzewczej LFP ( z osprzętem )</td>
+    <td>szt.</td>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>9</td>
+    <td>MIEDŹ CHŁODNICZA</td>
+    <td>kpl.</td>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>10</td>
+    <td>REGULATOR BEZPRZEWODOWY MITSUBISHI PAR-WT</td>
+    <td>szt.</td>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>11</td>
+    <td>STOJAK LUB WIESZAK POD POMPĘ CIEPŁA</td>
+    <td>szt.</td>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>12</td>
+    <td>MONTAŻ , DOJAZD , URUCHOMIENIE ORAZ SZKOLENIE UŻYTKOWNIKA</td>
+    <td>kpl.</td>
+    <td>1</td>
+  </tr>
+    </tbody>
+  </table>
+  `;
+  break;
+  
+  
+      }
 
   case "KAMEN-KOMPAKT-LUX":
 
