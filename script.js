121902f86c9f75952f30d58e914e4aff7095ebe0
JaMagik <114476897+JaMagik@users.noreply.github.com>
2025-01-13 12:53:34 +0100

    MIDEA

--- script.js
+++ script.js
@@ -128,6 +128,10 @@ let Nibe12S1;
 let Nibe12S2;
 let Nibe12DS1;
 let Nibe12DS2;
+let Midea1;
+let Midea2;
+let MideaDS1;
+let MideaDS2;
 
 async function loadImageData() {
     const imagesModule = await import('./images.js');
@@ -259,6 +263,11 @@ async function loadImageData() {
     Nibe12S2 = imagesModule.Nibe12S2;
     Nibe12DS1 = imagesModule.Nibe12DS1;
     Nibe12DS2 = imagesModule.Nibe12DS2;
+    MideaDS1 = imagesModule.MideaDS1;
+    MideaDS2 = imagesModule.MideaDS2;
+    Midea1 = imagesModule.Midea1;
+    Midea2 = imagesModule.Midea2;
+
 } 
 
 loadImageData();
@@ -332,6 +341,8 @@ function getSecondPageBackgroundImageByType(pdfType) {
             return Lazar2;
         case "ROTENSO":
             return R1;
+            case "MIDEA":
+            return Midea2;
         case "Kotlospaw Slimko Plus":
             return KTS2;
         case "KAMEN-KOMPAKT-LUX":
@@ -348,6 +359,7 @@ function getSecondPageBackgroundImageByType(pdfType) {
                 return Nibe12S1;
                 case "Kotlospaw slimko plus niski":
                     return DrSlimN2;
+                    
         default:
             
             return null; // lub jakiś domyślny obraz, jeśli potrzebujesz
@@ -406,6 +418,9 @@ function getBackgroundImageByType(pdfType) {
     } 
     else if (pdfType === "DREWKO-HYBRID") {
         return DrSlim1;
+        
+    }  else if (pdfType === "MIDEA") {
+        return Midea1;
         
     } 
     else if (pdfType === "NIBE12") {
@@ -527,6 +542,9 @@ function generatePDF() {
     
     const N12DS2 = `<div id="page" style="background-image: url('${Nibe12DS2}');"></div>`;
     
+    const MDS1= `<div id="page" style="background-image: url('${MideaDS1}');"></div>`;
+    const MDS2= `<div id="page" style="background-image: url('${MideaDS2}');"></div>`;
+
 
 
 
@@ -698,6 +716,11 @@ else if (pdfType === 'DREWKO-HYBRID')
             // Dodaj standardowe strony
             content = firstPageContent + secondPageContent+ thirdPageContent+N12DS1+N12DS2+MeetUsContent+FourthPageContent;
         }
+        else if (pdfType === 'MIDEA') 
+            {
+                // Dodaj standardowe strony
+                content = firstPageContent + secondPageContent+ thirdPageContent+MDS1+MDS2+MeetUsContent+FourthPageContent;
+            }
 
 else if (pdfType === 'ATLANTIC') {
     var selectedPower = document.getElementById('powerOptions').value;
@@ -1063,6 +1086,272 @@ function getTableContentByType(pdfType, power, bufferCapacity, tankCapacity) {
   </table>
   `};
   break;
+
+
+  case "MIDEA":
+    if (power === '12 kW') {
+      tableContent = `
+  <table id="customTable" border="1">
+  <tbody>
+  <tr>
+  <th>Numer</th>
+  <th>Nazwa towaru</th>
+  <th>Miara</th>
+  <th>Ilość</th>
+</tr>
+<tr>
+  <td>1</td>
+  <td>JEDNOSTKA WEWNĘTRZNA MIDEA M-THERMAL ARCTIC 12 kW</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>2</td>
+  <td>JEDNOSTKA ZEWNĘTRZNA M-THERMAL ARCTIC</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>3</td>
+  ${tankRow}
+</tr>
+<tr>
+    <td>4</td>
+    ${bufferRow}
+</tr>;
+<tr>
+  <td>5</td>
+  <td>ELEMENTY HYDRAULICZNE I ELEKTRYCZNE DO POMPY CIEPŁA</td>
+  <td>kpl.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>6</td>
+  <td>Grupa bezpieczeństwa CWU (6bar)</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>7</td>
+  <td>Grupa bezpieczeństwa C.0 (2.5 bar)</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>8</td>
+  <td>Pompa obiegowa do instalacji grzewczej LFP ( z osprzętem )</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>9</td>
+  <td>MIEDŹ CHŁODNICZA</td>
+  <td>kpl.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>10</td>
+  <td>REGULATOR BEZPRZEWODOWY MITSUBISHI PAR-WT</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>11</td>
+  <td>STOJAK LUB WIESZAK POD POMPĘ CIEPŁA</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>12</td>
+  <td>MONTAŻ , DOJAZD , URUCHOMIENIE ORAZ SZKOLENIE UŻYTKOWNIKA</td>
+  <td>kpl.</td>
+  <td>1</td>
+</tr>
+  </tbody>
+</table>
+`;
+break;
+
+
+
+
+
+
+
+
+
+
+    }
+
+    else if (power === '8 kW') {
+        tableContent = `
+    <table id="customTable" border="1">
+   <tbody>
+  <tr>
+  <th>Numer</th>
+  <th>Nazwa towaru</th>
+  <th>Miara</th>
+  <th>Ilość</th>
+</tr>
+<tr>
+  <td>1</td>
+  <td>JEDNOSTKA WEWNĘTRZNA MIDEA M-THERMAL ARCTIC 8 kW</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>2</td>
+  <td>JEDNOSTKA ZEWNĘTRZNA M-THERMAL ARCTIC</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>3</td>
+  ${tankRow}
+</tr>
+<tr>
+    <td>4</td>
+    ${bufferRow}
+</tr>;
+<tr>
+  <td>5</td>
+  <td>ELEMENTY HYDRAULICZNE I ELEKTRYCZNE DO POMPY CIEPŁA</td>
+  <td>kpl.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>6</td>
+  <td>Grupa bezpieczeństwa CWU (6bar)</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>7</td>
+  <td>Grupa bezpieczeństwa C.0 (2.5 bar)</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>8</td>
+  <td>Pompa obiegowa do instalacji grzewczej LFP ( z osprzętem )</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>9</td>
+  <td>MIEDŹ CHŁODNICZA</td>
+  <td>kpl.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>10</td>
+  <td>REGULATOR BEZPRZEWODOWY MITSUBISHI PAR-WT</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>11</td>
+  <td>STOJAK LUB WIESZAK POD POMPĘ CIEPŁA</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>12</td>
+  <td>MONTAŻ , DOJAZD , URUCHOMIENIE ORAZ SZKOLENIE UŻYTKOWNIKA</td>
+  <td>kpl.</td>
+  <td>1</td>
+</tr>
+  </tbody>
+  </table>
+  `;
+  break;
+    }
+    else if (power === '10 kW') {
+      tableContent = `
+  <table id="customTable" border="1">
+   <tbody>
+  <tr>
+  <th>Numer</th>
+  <th>Nazwa towaru</th>
+  <th>Miara</th>
+  <th>Ilość</th>
+</tr>
+<tr>
+  <td>1</td>
+  <td>JEDNOSTKA WEWNĘTRZNA MIDEA M-THERMAL ARCTIC 10 kW</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>2</td>
+  <td>JEDNOSTKA ZEWNĘTRZNA M-THERMAL ARCTIC</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>3</td>
+  ${tankRow}
+</tr>
+<tr>
+    <td>4</td>
+    ${bufferRow}
+</tr>;
+<tr>
+  <td>5</td>
+  <td>ELEMENTY HYDRAULICZNE I ELEKTRYCZNE DO POMPY CIEPŁA</td>
+  <td>kpl.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>6</td>
+  <td>Grupa bezpieczeństwa CWU (6bar)</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>7</td>
+  <td>Grupa bezpieczeństwa C.0 (2.5 bar)</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>8</td>
+  <td>Pompa obiegowa do instalacji grzewczej LFP ( z osprzętem )</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>9</td>
+  <td>MIEDŹ CHŁODNICZA</td>
+  <td>kpl.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>10</td>
+  <td>REGULATOR BEZPRZEWODOWY MITSUBISHI PAR-WT</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>11</td>
+  <td>STOJAK LUB WIESZAK POD POMPĘ CIEPŁA</td>
+  <td>szt.</td>
+  <td>1</td>
+</tr>
+<tr>
+  <td>12</td>
+  <td>MONTAŻ , DOJAZD , URUCHOMIENIE ORAZ SZKOLENIE UŻYTKOWNIKA</td>
+  <td>kpl.</td>
+  <td>1</td>
+</tr>
+  </tbody>
+</table>
+`}
+
+;
+break;
+
+
 
 
   case "Mitsubishi-hp":
@@ -8215,6 +8504,86 @@ case "VIESSMANN":
                 </tr>
                     </table>`;
                 break;
+
+                case "MIDEA":
+                    tableHtml = `
+                        <table border='1' id="kamanTable">
+                        <tr>
+                        <th>Lp.</th>
+                        <th>Nazwa</th>
+                        <th>Miara</th>
+                        <th>Cena</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Pompa do cyrkulacji z osprzętem i regulatorem czasowym</td>
+                        <td>szt.</td>
+                        <td>660zł</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Separator zanieczyszczeń magnetyczny (odmulnik)</td>
+                        <td>szt.</td>
+                        <td>580zł</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Pompa obiegowa do instalacji C.0 (LFP/WILO) z osprzętem</td>
+                        <td>szt.</td>
+                        <td>650zł</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td> Zbiornik z stali nierdzewnej</td>
+                        <td>szt.</td>
+                        <td>1250zł</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>Dodatkowy sterownik do zarządznia do drugą strefą</td>
+                        <td>szt.</td>
+                        <td>450zł</td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>Licznik energii elektrycznej 3f</td>
+                        <td>szt.</td>
+                        <td>540zł</td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>Kabel grzewczy z termostatem ( istnieje możliwość podpięcia do kanalizacji lub drenażu po wcześniejszych oględzinach i potwierdzeniu przez montera ) </td>
+                        <td>kpl.</td>
+                        <td>500zł</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>Wykonanie podbudowy ( fundamentu ) pod pompę ciepła: krawężniki przemysłowe ułożone na podsypce betonowej minimum B20 na głębokość 30-40 cm ( górna krawedź ułożona na równo z gruntem chłonnym)</td>
+                        <td>kpl.</td>
+                        <td>300zł</td>
+                    </tr>
+                    <tr>
+                        <td>9</td>
+                        <td>Doprowadzenie kabla siłowego do pompy ciepła ( liczone gdy odległość kabla zasilającego o odpowiednim przekroju jest większa niż 10 mb )</td>
+                        <td>mb.</td>
+                        <td>55zł</td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Demontaż starego źródła ciepła - polega na odłączeniu kotła od instalacji C.O i odsunięciu go tak aby nie przeszkadzał przy montażu pompy ciepła ( w tym samym pomieszczeniu ). Istnieje możliwość wywiezienia go poza pomieszczenie w którym był zamontowany jeżeli będzie to możliwe za pomocą wózka paletowego ( tzn. powierzchnia musi być równa bez progów i odpowiedniej szerokośći )</td>
+                        <td>kpl.</td>
+                        <td>500zł</td>
+                    </tr>
+                 <tr>
+                        <td>11</td>
+                        <td>BUFOR (SPRZĘGŁO HYDRAULICZNE) 40-140L + OSPRZĘT</td>
+                        <td>Kpl</td>
+                        <td>1400-1950 zł</td>
+                    </tr>
+                        </table>`;
+                    break;
+
+
                 case "Toshiba 1F":
                     tableHtml = `
                         <table border='1' id="kamanTable">
